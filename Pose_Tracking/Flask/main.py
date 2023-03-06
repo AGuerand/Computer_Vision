a0aa6a8d95a7c9ae73b13af9f17e2534a5ddfd27
Anthony GUERAND <73344943+AGuerand@users.noreply.github.com>
2023-03-06 11:36:28 +0100

    Flask + Pose Estimation

--- Pose_Tracking/Flask/main.py
+++ Pose_Tracking/Flask/main.py
@@ -1,9 +1,104 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, render_template, request, url_for, flash, redirect, Response
 import sqlite3
 from werkzeug.exceptions import abort
+import cv2
+import mediapipe as mp
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
 
 app = Flask(__name__)
 
+# TEST
+
+def generate_frames():
+    f = open("output.txt", "w")
+
+    # For static images:
+    IMAGE_FILES = []
+    BG_COLOR = (192, 192, 192) # gray
+    with mp_pose.Pose(
+        static_image_mode=True,
+        model_complexity=2,
+        enable_segmentation=True,
+        min_detection_confidence=0.5) as pose:
+        for idx, file in enumerate(IMAGE_FILES):
+            image = cv2.imread(file)
+            image_height, image_width, _ = image.shape
+            # Convert the BGR image to RGB before processing.
+            results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+            if not results.pose_landmarks:
+                continue
+            print(
+                f'Nose coordinates: ('
+                f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
+                f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height})'
+            )
+
+            annotated_image = image.copy()
+            # Draw segmentation on the image.
+            # To improve segmentation around boundaries, consider applying a joint
+            # bilateral filter to "results.segmentation_mask" with "image".
+            condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
+            bg_image = np.zeros(image.shape, dtype=np.uint8)
+            bg_image[:] = BG_COLOR
+            annotated_image = np.where(condition, annotated_image, bg_image)
+            # Draw pose landmarks on the image.
+            mp_drawing.draw_landmarks(
+                annotated_image,
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+            cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+            # Plot pose world landmarks.
+            mp_drawing.plot_landmarks(
+                results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+
+    # For webcam input:
+    n=0 #for print each frame
+    cap = cv2.VideoCapture(0) #Put 0 for webcam and also work for both photo and video (static)
+    with mp_pose.Pose(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            n+=1 # for print each frame
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
+
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = pose.process(image)
+            #print(results.pose_landmarks , "n = " ,n) #for coordonate each frame
+            #f.write(str(results.pose_landmarks.landmark[11]) + "n = " + str(n) + '\n' + '\n') #for coordonate each frame in output file /// [11] = landmark id
+            if results.pose_landmarks != None :
+                for i in range(0, len(results.pose_landmarks.landmark)) :
+                    f.write(f"coordonate of element index {i} is \n{results.pose_landmarks.landmark[i]}frame = {n} \n\n")
+                    f.flush()
+
+            # Draw the pose annotation on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            #image = flux sortie
+            mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+            ret, buffer = cv2.imencode('.jpg', image)
+            image = buffer.tobytes()
+            yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
+    f.close()
+    cap.release()
+
+# TEST
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -31,6 +126,10 @@ def index():
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/<int:post_id>')
 def post(post_id):
